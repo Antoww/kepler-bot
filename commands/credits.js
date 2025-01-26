7acbc3a2c1935008aa5f8cf633a4538a029d0066
Antow <27694299+Antoww@users.noreply.github.com>
2025-01-26 01:04:34 +0100

    add ayfri

--- commands/credits.js
+++ commands/credits.js
@@ -10,7 +10,7 @@ export async function execute(interaction) {
         .setColor('#0099ff')
         .setTitle('Crédits')
         .setImage('https://imgur.com/1CsxfGp.png')
-        .setDescription(`Icône du bot : [Freepik](https://www.flaticon.com/authors/freepik)\nBot développé par [Antow](https://github.com/Antoww/).`)
+        .setDescription(`Icône du bot : [Freepik](https://www.flaticon.com/authors/freepik)\nBot développé par [Antow](https://github.com/Antoww/).\nMerci à [Ayfri](https://ayfri.com/) pour sa patience et son aide. <3`)
         .setFooter({
             text: 'Demandé par ' + interaction.user.username,
             iconURL: interaction.user.displayAvatarURL({ dynamic: true })
