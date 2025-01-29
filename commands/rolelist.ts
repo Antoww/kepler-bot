9db80f4d604a0009dc1d252820bd118abb2bed37
Antow <27694299+Antoww@users.noreply.github.com>
2025-01-29 20:29:16 +0100

    Added rolelist

---
+++ commands/rolelist.ts
@@ -0,0 +1,32 @@
+import { type CommandInteraction, SlashCommandBuilder, EmbedBuilder } from "discord.js";
+
+export const data = new SlashCommandBuilder()
+    .setName('rolelist')
+    .setDescription('Liste les rôles du serveur.');
+
+export async function execute(interaction: CommandInteraction) {
+    if (!interaction.guild) {
+        await interaction.reply('Erreur : Impossible de récupérer les informations du serveur.');
+        return;
+    }
+
+    const roles = interaction.guild.roles.cache
+        .filter(role => role.name !== '@everyone')
+        .sort((a, b) => b.position - a.position)
+        .map(role => role.toString());
+
+    const embed = new EmbedBuilder()
+        .setColor('Blurple')
+        .setTitle('Liste des rôles')
+        .addFields(
+            //{ name: 'Rôles :', value: roles.join(', '), inline: true },
+            { name: 'Rôles affichés :', value: roles.length > 25 ? '25 premiers rôles' : roles.join(', ') },
+            { name: 'Nombre de rôles :', value: roles.length.toString(), inline: true },
+        )
+        .setFooter({
+            text: 'Demandé par ' + interaction.user.username,
+            iconURL: interaction.user.displayAvatarURL({ forceStatic: false })
+        })
+
+    await interaction.reply(( {embeds: [embed]} ));
+}
