fad263ce3c9762d9a17a8ec603fa629ad53b317c
Antoww <27694299+Antoww@users.noreply.github.com>
2025-08-03 17:31:24 +0200

    refactor(channelinfo): Remove author field from embed in channel info command

--- commands/utilitaires/channelinfo.ts
+++ commands/utilitaires/channelinfo.ts
@@ -16,10 +16,6 @@ export async function execute(interaction: CommandInteraction) {
     }
 
     const embed = new EmbedBuilder()
-        .setAuthor({ 
-            name: interaction.client.user?.username, 
-            iconURL: interaction.client.user?.displayAvatarURL({ forceStatic: false }) 
-        })
         .setColor('#0099ff')
         .setTitle(`Informations sur ${targetChannel.name}`)
         .addFields(
