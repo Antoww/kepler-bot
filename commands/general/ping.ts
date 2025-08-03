fc6fdaf8b4e07fbf12d3372fde5aa7e1c37b188a
Antoww <27694299+Antoww@users.noreply.github.com>
2025-08-03 16:13:33 +0200

    refactor(ping): Remove author information and color from ping command embed

--- commands/general/ping.ts
+++ commands/general/ping.ts
@@ -8,8 +8,6 @@ export async function execute(interaction: CommandInteraction) {
     const apiPing = Date.now() - interaction.createdTimestamp;
 
     const embed = new EmbedBuilder()
-        .setAuthor({ name: interaction.client.user?.username, iconURL: interaction.client.user?.displayAvatarURL({ forceStatic: false }) })
-        .setColor('#0099ff')
         .setTitle('Pong ! 🏓')
         .setDescription(`Latence du bot : ${botPing}ms\nLatence de l'API : ${apiPing}ms`)
         .setFooter({
